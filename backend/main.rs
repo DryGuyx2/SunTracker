5ff6d82321573d5f2f1fb4c32ed0df7491524a61
DryGuy <herman.wedvik@gmail.com>
2025-03-08 20:36:03 +0100

    Switch ui stack, add basic ui, improve error handling
    Switched ui stack to htmx + alpine js and made the ui function, also improved the error handling system when parsing dates.

--- backend/main.rs
+++ backend/main.rs
@@ -1,6 +1,6 @@
 mod data;
 
-use data::{TimeStamp, Date, Month};
+use data::{Date, TimeStamp};
 
 use axum::{
     extract::{Path, State},
@@ -8,13 +8,18 @@ use axum::{
     Router,
 };
 
+use tower_http::services::ServeFile;
+
 use std::{collections::HashMap, io::Read};
 
 #[cfg(not(target_arch = "wasm32"))]
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
     let mut site_contents = String::new();
-    std::fs::File::open("site.html").unwrap().read_to_string(&mut site_contents).unwrap();
+    std::fs::File::open("site.html")
+        .unwrap()
+        .read_to_string(&mut site_contents)
+        .unwrap();
 
     //let site_contents = data::fetch_site().await?;
     //let target_date: Date = Date::new(String::from("Des"), 3u8).unwrap();
@@ -24,7 +29,8 @@ async fn main() -> Result<(), reqwest::Error> {
     let state: AppState = AppState::new(sunsets);
 
     let app = Router::new()
-        .route("/{month}/{day}", get(get_timestamp))
+        .route_service("/", ServeFile::new("../frontend/index.html"))
+        .route("/api/{month}/{day}", get(get_timestamp))
         .with_state(state);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:80").await.unwrap();
@@ -40,27 +46,30 @@ struct AppState {
 
 impl AppState {
     fn new(sunset_table: HashMap<Date, TimeStamp>) -> AppState {
-        AppState {sunset_table}
+        AppState { sunset_table }
     }
     fn get_sunset(&self, date: &Date) -> Option<&TimeStamp> {
         self.sunset_table.get(date)
     }
 }
 
-
-async fn get_timestamp(State(state): State<AppState>, Path((month, day)): Path<(String, String)>) -> String {
+async fn get_timestamp(
+    State(state): State<AppState>,
+    Path((month, day)): Path<(String, String)>,
+) -> String {
+    println!("Recieved request: {:?}", (&month, &day));
     let parsed_day: u8 = match day.parse() {
         Ok(num) => num,
-        Err(_) => return String::from("None"),
+        Err(_) => return String::from("Failed parsing day."),
     };
 
     let target_date: Date = match Date::new(month, parsed_day) {
         Ok(d) => d,
-        Err(_) => return String::from("None"),
+        Err(e) => return format!("{}", e),
     };
 
     match state.get_sunset(&target_date) {
         Some(t) => t.to_string(),
-        None => String::from("None"),
+        None => String::from("Date not found in datetime index."),
     }
 }
